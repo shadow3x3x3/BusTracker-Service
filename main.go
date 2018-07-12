606033f2717e5428ed80308d7b1d123730b4b437
Jeff Chiu <shadow3x3x3@gmail.com>
2018-07-12 12:03:06 +0800

    Add basic server

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type trackerPoint struct {
+	ID        string  `json:"id"`
+	TimeStamp string  `json:"timestamp"`
+	Longitude float64 `json:"longitude"`
+	Latitude  float32 `json:"latitude"`
+}
+
+func getTrackers(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   "null", // TODO: Fetch data from db
+	})
+}
+
+func postTrackers(c *gin.Context) {
+	p := trackerPoint{}
+
+	if err := c.BindJSON(&p); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":  "json decoding : " + err.Error(),
+			"status": http.StatusBadRequest,
+		})
+
+		return
+	}
+	// TODO: Error handles
+
+	fmt.Printf("Point: %+v\n", p)
+
+	// TODO: Insert data into db
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+	})
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/trackers", getTrackers)
+	r.POST("/trackers", postTrackers)
+
+	r.Run()
+}
